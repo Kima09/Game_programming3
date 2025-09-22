2a4a2bfdb776480a3d05c875bbedd99783799cf4
Kima09 <hkdavidl666@gmail.com>
2025-09-22 19:47:59 +0900

    (2025.9.22) 템플릿

---
+++ Class7th (Encapsulation)/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include "../Program/stdafx.h"
+#include "../Program/Vector2.h"
+
+int main()
+{
+#pragma region 캡슐화
+	//객체의 속성과 기능을 하나의 단위로 합친 후
+	//외부에서 필요한 정보만 접근할 수 있도록 제한하는 기능입니다.
+
+	Vector2 point1D(1, 0);
+	Vector2 point2D(0, 1);
+
+	Vector2 direction = point1D + point2D;
+
+	Vector2 position = direction++;
+
+	cout << position.X() << endl;
+	cout << position.Y() << endl;
+
+	cout << direction.X() << endl;
+	cout << direction.Y() << endl;
+
+#pragma endregion
+
+
+
+
+	return 0;
+}
--- Program/Vector2.h
+++ Program/Vector2.h
@@ -6,7 +6,21 @@ private:
 	int y;
 public:
 	Vector2(int x, int y);
-	Vector2 & operator + (const Vector2 & clone);
 
+	const int& X();
+	const int& Y();
+
+	const Vector2 & operator + (const Vector2 & clone);
+	const Vector2& operator - (const Vector2& clone);
+	const Vector2& operator * (const Vector2& clone);
+	const Vector2& operator / (const Vector2& clone);
+
+	const Vector2& operator ++();
+	const Vector2& operator --();
+//후위
+
+	const Vector2& operator ++(int);
+	const Vector2& operator --(int);
+//전위
 };
 
