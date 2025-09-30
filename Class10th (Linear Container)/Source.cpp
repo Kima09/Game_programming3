64fbaae543730330bb4f8854e0128ed30d642f9f
Kima09 <hkdavidl666@gmail.com>
2025-09-30 19:46:29 +0900

    (2025.9.30) 컨테이너 어댑터

---
+++ Class10th (Linear Container)/Source.cpp
@@ -0,0 +1,94 @@
+﻿#include "../Program/stdafx.h"
+#include <vector>
+#include <list>
+#include <deque>
+
+int main()
+{
+#pragma region 선형 컨테이너
+	//데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 컨테이너입니다.
+
+#pragma region vector container
+
+	//vector<int> vector; //템플릿. <형태> 변수명.
+	////동적 배열이므로 인덱스 접근 가능하다.
+	//
+	//vector.reserve(8);//용량을 미리 확보하는 함수
+	//
+	//vector.push_back(10);
+	//cout<<vector.capacity()<<" "; //사이즈 반환 함수
+	//vector.push_back(20);
+	//cout << vector.capacity() << " ";
+	//vector.push_back(30);
+	//cout << vector.capacity() << " ";
+	//vector.push_back(40);
+	//cout << vector.capacity() << " ";
+	//vector.push_back(50); //데이터를 삽입하는 함수
+	//cout << vector.capacity() << " ";
+	//
+	//vector.pop_back(); //마지막 원소를 삭제한다. 출력하는 함수는 아니다.
+	//
+	//for (int i = 0;i < vector.size();i++)
+	//{
+	//	cout << vector[i] << " ";
+	//}
+	//
+	//vector.clear();
+	//cout << "vector의 size값: " << vector.size() << endl;
+	//cout << "vector의 capacity값: " << vector.capacity() << endl;
+//0, 8이 출력된다.
+
+//capacity의 결과 1, 2, 3, 4, 6이 출력된다.
+//capacity는 VS의 경우 약 1.5배씩 증가하기 때문이다. 이는 프로그램마다 다를 수 있다.
+
+#pragma endregion
+
+#pragma region list container
+
+	std::list<int> list;
+
+	list.push_back(20);
+	list.push_back(30);
+
+	list.push_front(10);
+
+	list.pop_front();
+
+	list.push_front(1);
+
+	list.remove(20);
+
+	//for(const자료형 변수명 데이터컨테이너)
+
+#pragma endregion
+
+#pragma region deque container
+
+	//std::deque<int> deque;
+	//
+	//deque.push_back(10);
+	//deque.push_back(20);
+	//deque.push_back(30);
+	//
+	//deque.push_front(5);
+	//deque.push_front(1);
+	//
+	//deque.push_back(40);
+	//deque.push_back(50);
+	//
+	//deque.pop_front();
+	//deque.pop_back();
+	//
+	//int index;
+	//
+	//cout << "index: ";
+	//cin >> index;
+	//cout << deque.at(index) << endl;
+
+#pragma endregion
+
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,63 +1,33 @@
 ﻿#include "stdafx.h"
-#include <vector>
-#include <list>
+#include <stack>
+#include <queue>
 
 int main()
 {
-#pragma region 선형 컨테이너
-//데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 컨테이너입니다.
+#pragma region 컨테이너 어댑터
+//기존 컨테이너의 인터페이스를 제한하여 만든 기능이
+//제한되거나 변경된 컨테이너입니다.
 
-#pragma region vector container
+#pragma region stack container
+	std::stack<int> stack;
 
-	//vector<int> vector; //템플릿. <형태> 변수명.
-	////동적 배열이므로 인덱스 접근 가능하다.
-	//
-	//vector.reserve(8);//용량을 미리 확보하는 함수
-	//
-	//vector.push_back(10);
-	//cout<<vector.capacity()<<" "; //사이즈 반환 함수
-	//vector.push_back(20);
-	//cout << vector.capacity() << " ";
-	//vector.push_back(30);
-	//cout << vector.capacity() << " ";
-	//vector.push_back(40);
-	//cout << vector.capacity() << " ";
-	//vector.push_back(50); //데이터를 삽입하는 함수
-	//cout << vector.capacity() << " ";
-	//
-	//vector.pop_back(); //마지막 원소를 삭제한다. 출력하는 함수는 아니다.
-	//
-	//for (int i = 0;i < vector.size();i++)
-	//{
-	//	cout << vector[i] << " ";
-	//}
-	//
-	//vector.clear();
-	//cout << "vector의 size값: " << vector.size() << endl;
-	//cout << "vector의 capacity값: " << vector.capacity() << endl;
-//0, 8이 출력된다.
+	stack.push(10);
+	stack.push(20);
+	stack.push(30);
+	stack.push(40);
+	stack.push(50);
 
-//capacity의 결과 1, 2, 3, 4, 6이 출력된다.
-//capacity는 VS의 경우 약 1.5배씩 증가하기 때문이다. 이는 프로그램마다 다를 수 있다.
+	while (stack.empty() == false)
+	{
+		cout << stack.top() << endl;
+		stack.pop();
+	}
 
 #pragma endregion
 
-#pragma region list container
+#pragma region queue container
 
-	std::list<int> list;
-
-	list.push_back(20);
-	list.push_back(30);
-
-	list.push_front(10);
-
-	list.pop_front();
-
-	list.push_front(1);
-
-	list.remove(20);
-
-	//for(const자료형 변수명 데이터컨테이너)
+	std::queue<int> queue;
 
 #pragma endregion
 
